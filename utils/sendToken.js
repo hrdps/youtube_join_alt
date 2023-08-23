42b2c2d43581c66430ee87dd6898e73e6bc6c561
Hardeep Singh <hardeep.space@gmail.com>
2023-08-23 12:06:29 +0530

    cors changes

--- utils/sendToken.js
+++ utils/sendToken.js
@@ -3,7 +3,7 @@ export const sendToken = (res, user, message, statusCode = 200) => {
   const options = {
     expires: new Date(Date.now() + 15 * 24 * 60 * 60 * 1000),
     sameSite: 'none',
-    secure: false,
+    secure: true,
     path: '/',
   };
   res.status(statusCode).cookie('token', token, options).json({
