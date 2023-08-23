609c90ec95f8b008a7ea633aa4a301624b1473c1
Hardeep Singh <hardeep.space@gmail.com>
2023-08-23 11:38:01 +0530

    cookie error changes4

--- utils/sendToken.js
+++ utils/sendToken.js
@@ -3,15 +3,15 @@ export const sendToken = (res, user, message, statusCode = 200) => {
   const options = {
     // expires: new Date(Date.now() + 15 * 24 * 60 * 60 * 1000),
     httpOnly: false,
-    // sameSite: 'none',
+    sameSite: 'none',
     // secure: true,
     maxAge: 15 * 24 * 60 * 60 * 1000, // AUTH_SECRET_TOKEN_LIFE is in seconds, convert seconds to ms
     httpOnly: true,
     path: '/',
-    secure: false,
+    secure: true,
     domain: 'localhost',
   };
-  res.status(statusCode).cookie('token', token, options).json({
+  res.header().status(statusCode).cookie('token', token, options).json({
     success: true,
     message,
     user,
