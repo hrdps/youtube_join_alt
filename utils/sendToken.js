12e64d7d5054357c411a8185ea43ac6ff825f2d3
Hardeep Singh <hardeep.space@gmail.com>
2023-08-25 22:44:11 +0530

    cookie changes

--- utils/sendToken.js
+++ utils/sendToken.js
@@ -1,10 +1,8 @@
 export const sendToken = (res, user, message, statusCode = 200) => {
   const token = user.getJWTToken();
   const options = {
-    expires: new Date(Date.now() + 15 * 24 * 60 * 60 * 1000),
     sameSite: 'none',
     secure: true,
-    path: '/',
   };
   res.status(statusCode).cookie('token', token, options).json({
     success: true,
