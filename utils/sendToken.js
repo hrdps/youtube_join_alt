78a9974ce55b5a8be773db308eb55da593ef5a9a
Hardeep Singh <hardeep.space@gmail.com>
2023-08-25 23:44:56 +0530

    cookie option changes

--- utils/sendToken.js
+++ utils/sendToken.js
@@ -3,7 +3,7 @@ export const sendToken = (res, user, message, statusCode = 200) => {
   const options = {
     sameSite: 'none',
     secure: true,
-    domain: 'http://localhost:3000',
+    maxAge: 7 * 24 * 60 * 60 * 1000,
   };
   res.status(statusCode).cookie('token', token, options).json({
     success: true,
